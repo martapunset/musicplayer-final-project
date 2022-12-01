f61fcb5f970c75b8cb57c7011f517988a037cc2d
Marta Punset <martapunset10@gmail.com>
2022-12-01 14:19:44 +0100

    styled input component

--- vite-project/src/auth/pages/AuthHomePage.jsx
+++ vite-project/src/auth/pages/AuthHomePage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button } from '../../ui/Buttons.style'
+import { Button , Input } from '../../ui/Buttons.style'
 import { GridRegisterLogin } from '../../ui/Gridstyle'
 import { Img, Img2 } from '../../ui/Img'
 
@@ -8,7 +8,8 @@ export const AuthHomePage = () => {
     return (
         <>
             <GridRegisterLogin>
-            <Img></Img>
+          <Img></Img>
+          <Input />
             <Button>login</Button>
             <Button>register</Button>
             <Img2></Img2>
--- vite-project/src/auth/pages/LoginPage.jsx
+++ vite-project/src/auth/pages/LoginPage.jsx
@@ -1,5 +1,17 @@
-import React from "react";
+import React from "react"
+import { Button, Input } from "../../ui/Buttons.style";
+import { GridRegisterLogin } from "../../ui/Gridstyle";
+
 
 export const LoginPage = () => {
-  return <div>loginPage</div>;
+  return <>
+    <div>loginPage</div>;
+    <GridRegisterLogin>
+      <form>
+      
+        <Input type="text" placeholder="Name"/>
+        <Button>SingIn</Button>
+      </form>
+      </GridRegisterLogin>
+    </>
 };
