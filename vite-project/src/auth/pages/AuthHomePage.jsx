f4d0a39554d536d77b268dd42824a6515bb99787
Elirv <elizabeth.ramos101@hotmail.com>
2022-12-01 12:32:40 +0000

    -

--- vite-project/src/auth/pages/AuthHomePage.jsx
+++ vite-project/src/auth/pages/AuthHomePage.jsx
@@ -1,15 +1,18 @@
-import { GridRegisterLogin } from "../../ui/Gridstyle";
-import { Img, Img2 } from "../../ui/Img";
+import React from 'react'
+import { Button } from '../../ui/Buttons.style'
+import { GridRegisterLogin } from '../../ui/Gridstyle'
+import { Img, Img2 } from '../../ui/Img'
+
 
 export const AuthHomePage = () => {
-  return (
-    <>
-      <GridRegisterLogin>
-        <Img></Img>
-        <button>login</button>
-        <button>register</button>
-        <Img2></Img2>
-      </GridRegisterLogin>
-    </>
-  );
-};
+    return (
+        <>
+            <GridRegisterLogin>
+            <Img></Img>
+            <Button>login</Button>
+            <Button>register</Button>
+            <Img2></Img2>
+            </GridRegisterLogin>
+        </>
+    )
+}
