e9a542dd895bac60ce6013a129905c6294eed841
Elirv <elizabeth.ramos101@hotmail.com>
2022-12-02 12:20:50 +0000

    login

--- vite-project/src/components/Login.jsx
+++ vite-project/src/components/Login.jsx
@@ -8,28 +8,28 @@ export const Login = () => {
         <Container className="contenedor">
 
             <Header className="header">
-                <IconBack>back</IconBack><img src={facebook} alt="icon" width="30" />
+                <IconBack>back</IconBack><img src={facebook} alt="icon" width="30" /> {/* LINK en back */}
             </Header>
 
             <Main className="contenido">
                 <Title> Sign In</Title>
-                <Textsmall>If you need any support click here</Textsmall>
+                <Textsmall>If you need any support click here</Textsmall> {/* LINK */}
             </Main>
 
-            <Sidebar className="sedebar">
+            <Sidebar className="sidebar">
                 <input placeholder="Enter username or email" />
             </Sidebar>
 
             <Widget className="widget-1">
                 <Column>
                     <input placeholder="Password" />
-                    <Textsmall>Recovery password</Textsmall>
+                     <Textsmall>Recovery password</Textsmall> {/* LINK */}
                 </Column>
             </Widget>
             <Widget className="widget-2">
                 <Column>
                     <button type="input">Sign In</button>
-                    <Textsmall>or</Textsmall>
+                    <Textsmall>or sign in with</Textsmall>
                     <div>
                         <img src={facebook} alt="icon" width="30" />
                         <img src={facebook} alt="icon" width="30" />
@@ -38,7 +38,7 @@ export const Login = () => {
                 </Column>
             </Widget>
 
-            <Footer className="footer"><Textsmall>Not a member ? register now</Textsmall></Footer>
+            <Footer className="footer"><Textsmall>Not a member ? register now</Textsmall></Footer> {/* LINK */}
 
         </Container>
     )
--- vite-project/src/styles/TextStyle.js
+++ vite-project/src/styles/TextStyle.js
@@ -18,7 +18,7 @@ font-style: normal;
 font-weight: 400;
 font-size: 12px;
 line-height: 14px;
-text-transform: capitalize;
+${'' /* text-transform: capitalize; */}
 text-align: center;
 color: #E1E1E1;
 text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
