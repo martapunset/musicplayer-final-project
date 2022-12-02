c08b28a99e4a81bba10791ef61a9454a151f7a8a
Elirv <elizabeth.ramos101@hotmail.com>
2022-12-02 11:44:12 +0000

    login

--- vite-project/src/styles/model.js
+++ vite-project/src/styles/model.js
@@ -11,14 +11,13 @@ grid-template-rows: repeat(4, auto);
 `
 
 export const Header = styled.header`
-background: blue;
+${'' /* background: blue; */}
 color: #fff;
 grid-area: header;
 
 & > h1 {
     margin-left: 2%;
 }
-
 `
 
 export const Main = styled.main`
@@ -27,25 +26,20 @@ grid-area: contenido;
 `
 
 export const Sidebar = styled.aside`
-background: orange;
+${'' /* background: orange; */}
 min-height: 100px;
 grid-area: sidebar;
-
 /*flexbox para acomodar elementos*/
-
 display: flex;
 align-items: center;
 justify-content: center;
 `
 
 export const Widget = styled.div`
-
-background: orchid;
+${'' /* background: orchid; */}
 height: 100px;
 grid-area: ${props => props.className === "widget-1" ? "widget-1" : "widget-2"}; 
-
 /*flexbox para acomodar elementos*/
-
 display: flex;
 align-items: center;
 justify-content: center;
@@ -53,7 +47,7 @@ justify-content: center;
 
 export const Footer = styled.footer`
 padding: 20px;
-background: maroon;
+${'' /* background: maroon; */}
 color: #fff;
 grid-area: footer;
 `
@@ -62,5 +56,4 @@ export const Column = styled.div`
 display: flex;
 flex-direction: column;
 align-items: center;
-
 `
