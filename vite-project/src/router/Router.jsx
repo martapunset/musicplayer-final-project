f55a3c9f4d399e206d011732192e5dabb4d8f95d
Lokesh Pereiro <lpdraft001@gmail.com>
2022-12-06 13:26:33 +0100

    app pages

--- vite-project/src/router/Router.jsx
+++ vite-project/src/router/Router.jsx
@@ -1,5 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LoginPage, RegisterPage, AuthHomePage } from "../auth/pages/index.js";
+import { ProfilePage, SearchPage, WishPage } from "../pages";
+import { EditProfile } from "../components";
+
 const Router = () => {
   return (
     <>
@@ -8,6 +11,12 @@ const Router = () => {
           <Route path="/" element={<AuthHomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
+          // Inside APP pages
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/profile/:editId" element={<EditProfile />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/wish" element={<WishPage />} />
         </Routes>
       </BrowserRouter>
     </>
