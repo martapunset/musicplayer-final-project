32c888fb795bd5e2a9d6cda8f3e07ce75d136dd5
Marta Punset <martapunset10@gmail.com>
2022-12-06 11:57:02 +0100

    changes in authhomepage

--- vite-project/src/auth/pages/AuthHomePage.jsx
+++ vite-project/src/auth/pages/AuthHomePage.jsx
@@ -1,51 +1,52 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-import {   GlobalStyle } from '../../ui'
-import { Header, Sidebar, Container, Widget, IconBack, Title, Textsmall, Spam, Column, Input, Button, Footer, Main } from '../../ui'
-
+import { GlobalStyle, LinkStyled } from "../../ui";
+import {
+  Header,
+  Sidebar,
+  Container,
+  Widget,
+  IconBack,
+  Title,
+  Textsmall,
+  Spam,
+  Column,
+  Input,
+  Button,
+  Footer,
+  Main,
+} from "../../ui";
 
 export const AuthHomePage = () => {
-    return (
-        <>
-            <GlobalStyle />
-         
+  return (
+    <>
+      <GlobalStyle />
 
-{/* 
+      {/* 
             <Container className="contenedor" style={{ 
       backgroundImage: `url("homeBackground.jpeg") `
     }}> */}
-   <Container>
-            <Header className="header">
+      <Container>
+        <Header className="header"></Header>
+        <Main className="contenido">
+          <Column>
+            <Button><Link to="/login">Login</Link></Button>
 
-                
-            </Header>
-
-            <Main className="contenido">
-               
-            </Main>
-
-           
-            <Widget className="widget-1">
-                    <Column>
-                    <Link to="/login"><Button>login</Button></Link>
-            <Link to="/register"><Button>register</Button></Link>
-                
-                </Column>
-            </Widget>
-            <Widget className="widget-2">
-                
-                
-            </Widget>
-            <Footer className="footer">
-            <Column>
-                    
-                    
-                </Column><Textsmall> Not a member ? <Spam >Register now</Spam></Textsmall></Footer> {/* LINK */}
-
-
-            
-        </Container>
-        </>
-    )
-}
+            <LinkStyled to="/register">Register</LinkStyled>
+          </Column>
+        </Main>
+        <Widget className="widget-1"></Widget>
+        <Widget className="widget-2"></Widget>
+        <Footer className="footer">
+          <Column></Column>
+          <Textsmall>
+            {" "}
+            Not a member ? <Spam>Register now</Spam>
+          </Textsmall>
+        </Footer>{" "}
+        {/* LINK */}
+      </Container>
+    </>
+  );
+};
