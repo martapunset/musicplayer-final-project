f9fa13900c8e13dcf5e52d5c568d298a9e6c86c1
tonymg1 <tonymartingaldeano@gmail.com>
2022-12-13 12:21:22 +0100

    register front

--- vite-project/src/components/RegisterForm.jsx
+++ vite-project/src/components/RegisterForm.jsx
@@ -1,75 +1,111 @@
 import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+import { MdArrowBackIos } from "react-icons/md";
+import facebook from "../assets/img/face.png";
 import { postUsers, getUsers } from "../api/postUsers";
-import { Button, InputSmall } from "../ui";
 
-import { Form } from "../ui/Form";
+import { Form, InputSmall, Button, Container, Header, Widget, Textsmall, IconBack, Column, Footer } from "../ui";
+import { ResponsiveStyles } from "../ui/homegrid/ResponsiveStyles";
+// import {
+//   Header,
+//   Sidebar,
+//   Container,
+//   Widget,
+//   IconBack,
+//   Title,
+//   Textsmall,
+//   Spam,
+//   Column,
+//   Input,
+//   Button,
+//   Footer,
+//   Main,
+//   InputSmall,
+//   Form
+// } from "../../ui";
 
 export const RegisterForm = () => {
 
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm();
-  
-  const onSubmit = (data) => {
-//falta destructuring data
-    if (data.password !== data.confirmPassword) {return alert('password doesn´t match')}
-    const validateUser = async() => {
-      const user = await(getUsers(data.email))
-      user ? alert('this user already exists') : postUsers(data);
-      
-    };
-    validateUser();
+  const { register, handleSubmit } = useForm();
+
+  const postUser = async (data) => {
+    try {
+      const response = await fetch("http://localhost:4000/users",
+
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data)
+
+        })
+      const created = await response.json();
+      console.log('succesful addition: ', created)
+
+    }
+
+
+    catch (error) {
+      console.log(error)
+    }
   }
+  const onSubmit = (data) => {
 
-  console.log(watch("name")); // watch input value by passing the name of it
-  //  "handleSubmit" will validate your inputs before invoking "onSubmit"
+    console.log(data)
+    postUser({ ...data, id: Date.now() })
 
-
+  }
   return (
     <>
-      <Form onSubmit={handleSubmit(onSubmit)}>
-        {/* register your input into the hook by invoking the "register" function */}
-        <InputSmall
-          type="text" {...register("name", { required: true })}
-          placeholder="Name"
-        />
 
-        {/* include validation with required or other standard HTML validation rules */}
-        <InputSmall
-          type="text" {...register("first_name", { required: true })}
-          placeholder="First name"
-        />
-        {errors.exampleRequired && <span>This field is required</span>}
+      <ResponsiveStyles />
+      <Container className="contenedor">
+        <Header className="header">
+          <Link to="/">
+            <IconBack>
+              <MdArrowBackIos />
+            </IconBack>
+          </Link>
+          <center>
+            <img src={facebook} alt="icon" width="30" />
+          </center>
+        </Header>
+        <Widget className="widget-1">
+          <Column>
+            <h1>Register</h1>
+            <p>If you need any support <Link to="/support">Click here</Link></p>
+            <Form action="" onSubmit={handleSubmit(onSubmit)}>
+              <Column>
+                <InputSmall type="text" name="name" id="" placeholder='Name' {...register("name")} />
+                <InputSmall type="text" name="last_name" id="" placeholder='Last Name' {...register("last_name")} />
+                <InputSmall type="email" name="email" id="" placeholder='Email' {...register("email")} />
+                <InputSmall type="text" name="address" id="" placeholder='Adress' {...register("address")} />
+                <InputSmall type="password" name="password" id="" placeholder='Password' {...register("password")} />
+                <InputSmall type="password" name="confirm_password" id="" placeholder='Confirm Password' {...register("confirmPassword")} />
+                <Button type="submit"> Save</Button>
+              </Column>
+            </Form>
+          </Column>
+          </Widget>
+          <Footer className="abajo">
+            <p>or sign in with</p>
+            <img src={facebook} alt="icon" width="30" />
+            <img src={facebook} alt="icon" width="30" />
+            <img src={facebook} alt="icon" width="30" />
 
-        <InputSmall
-          type="email"{...register("email", { required: true})}
-          placeholder="Email"
-        />
-        {errors.exampleRequired && <span>This field is required</span>}
-
-        <InputSmall
-          type="text" {...register("address", { required: true })}
-          placeholder="Address"
-        />
-        {errors.exampleRequired && <span>This field is required</span>}
-
-        <InputSmall
-          type="password" {...register("password", { required: true })}
-          placeholder="Password"
-        />
-        {errors.exampleRequired && <span>This field is required</span>}
-
-        <InputSmall
-          type="password" {...register("confirmPassword", { required: true })}
-          placeholder="Confirm Password"
-        />
-        {errors.exampleRequired && <span>This field is required</span>}
-
-        <Button type="submit"> Save</Button>
-      </Form>
+            <footer>
+              <h6>
+                {" "}
+                Do you have an account ?{" "}
+                <Link to="/login">
+                  <p>Register now</p>
+                </Link>
+              </h6>
+            </footer>
+          </Footer>
+       
+      </Container>
     </>
-  );
+  )
 };
