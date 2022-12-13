9710270a661bbfa88afc242ce563f6c38e78296a
Lokesh Pereiro <lpdraft001@gmail.com>
2022-12-13 13:55:23 +0100

    created searchpage mapping

--- vite-project/src/pages/SearchPage.jsx
+++ vite-project/src/pages/SearchPage.jsx
@@ -1,33 +1,29 @@
 import { HomeNavBar } from "../components/HomeNavBar";
-
-
-import {
-  Header,
-  Footer,
-  Container,
-  Sidebar,
-  Main,
-  Widget,
-} from "../ui/model";
-
+import { ResponsiveStyles } from "../ui/homegrid/ResponsiveStyles";
+import { BsSearch } from "react-icons/bs";
+import { MdClear } from "react-icons/md";
+import { Header, Footer, Container, Sidebar, Main, Widget } from "../ui/model";
+import "./css/tem.css";
 export const SearchPage = () => {
   return (
     <>
-      <GlobalStyle />
+      <ResponsiveStyles />
       <Container className="contenedor">
-        <Header className="header">
+        <div className="containerSearch">
           <h1>Search Page</h1>
-        </Header>
-
-        <Main className="contenido">
-          <h2>test</h2>
-          <p>lorem</p>
-        </Main>
-
-        <Sidebar className="sedebar">Sidebar</Sidebar>
-
-        <Widget className="widget-1">widget-1</Widget>
-        <Widget className="widget-2">widget-2</Widget>
+          <div className="searchInputBox">
+            <button className="icons">
+              <BsSearch />
+            </button>
+            <input type="text" placeholder="Search for songs and playlists" />
+            <button className="icons">
+              <MdClear />
+            </button>
+          </div>
+        </div>
+        <div className="resultSearch">
+          <div className="songs_container">{}</div>
+        </div>
 
         <Footer className="footer">
           <HomeNavBar />
