a103ba6221fb41b70f96690db09b841696460782
Marta Punset <martapunset10@gmail.com>
2022-12-14 13:08:04 +0100

    profile page

--- vite-project/src/pages/ProfilePage.jsx
+++ vite-project/src/pages/ProfilePage.jsx
@@ -1,37 +1,76 @@
 import { HomeNavBar } from "../components/HomeNavBar";
 
 
+import React from 'react'
+import {InputProfile, Input2Profile, Footer, IconFooter, LogoProfile, UserProfile,UserImg, BtnEdit, Texto} from '../ui/probando';
+import {Button, } from '../ui/Buttons.style'
+import {GlobalStyle } from '../ui/globalStyles'
+import {Img, Img2} from '../ui/Img'
+import logo from "../assets/img/SpotifyVector.png";
+
+
+
 import {
   Header,
-  Footer,
+
   Container,
   Sidebar,
   Main,
   Widget,
 } from "../ui/model";
+
 export const ProfilePage = () => {
   return (
     <>
-     
+     <ResponsiveStyles />
       <Container className="contenedor">
+
         <Header className="header">
-          <h1>Profile Page</h1>
+          <Link to="/">
+            <IconBack>
+              <MdArrowBackIos />
+            </IconBack>
+          </Link>
+          <center>
+            <img src={facebook} alt="icon" width="30" />
+          </center>
         </Header>
 
-        <Main className="contenido">
-          <h2>test</h2>
-          <p>lorem</p>
-        </Main>
+        <Widget className="widget-1">
+                    <Column>
+                    <UserProfile>
+          <UserImg src="https://github.com/OlgaKoplik/CodePen/blob/master/profile.jpg?raw=true" />
+        <BtnEdit><Texto>Edit Profile</Texto></BtnEdit>
+      </UserProfile>
+      
+        <LogoProfile><img src={logo} alt="Logo" /></LogoProfile>
+       
+        <InputProfile placeholder='user@mail.com'/>
+        <Input2Profile placeholder='Username'/>
+      
+        <Button>Logout</Button>  
+                    </Column>
+                </Widget>
 
-        <Sidebar className="sedebar">Sidebar</Sidebar>
+       
+           
 
-        <Widget className="widget-1">widget-1</Widget>
-        <Widget className="widget-2">widget-2</Widget>
+     
 
-        <Footer className="footer">
-          <HomeNavBar />
-        </Footer>
-      </Container>
-    </>
-  );
-};
+        
+        <HomeNavBar/>
+        </ Container >
+      
+      </>
+      
+      )
+  }
+
+      
+
+
+  
+
+      
+
+
