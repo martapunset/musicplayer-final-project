c40f6eaf04daa77536ca64c768953defd18be0e0
Marta Punset <martapunset10@gmail.com>
2022-12-14 13:15:15 +0100

    profile page

---
+++ vite-project/src/ui/probando.js
@@ -0,0 +1,125 @@
+import styled from "styled-components"
+import user from "../assets/img/SpotifyVector.png";
+
+
+export const InputProfile = styled.input`
+/* Rectangle 25 */
+
+
+box-sizing: border-box;
+position: absolute;
+width: 334px;
+height: 80px;
+
+left: 53px;
+top: 356px;
+padding: 10px;
+//background: #D9D9D9;
+background-color: white;
+opacity: 0.14;
+border: 1px solid white;
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+border-radius: 30px;`
+
+
+export const Input2Profile = styled.input`
+/* Rectangle 25 */
+
+
+box-sizing: border-box;
+position: absolute;
+width: 334px;
+height: 80px;
+
+left: 53px;
+top: 452px;
+padding: 10px;
+//background: #D9D9D9;
+background-color: white;
+opacity: 0.14;
+border: 1px solid white;
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+border-radius: 30px;`
+
+
+export const Footer = styled.footer`
+position: absolute;
+width: 428px;
+height: 73px;
+left: 0px;
+background-color: #2F2C79;
+top: 853px;`
+
+
+export const IconFooter = styled.div`
+display: flex;
+flex-direction: row;
+align-items: flex-start;
+padding: 0px;
+gap: 63px;
+
+position: absolute;
+width: 330.33px;
+height: 28px;
+left: 48.29px;
+top: 876px;
+`
+
+
+export const LogoProfile = styled.div`
+position: absolute;
+left: 37.38%;
+right: 37.38%;
+top: 3.02%;
+bottom: 93.41%;
+`
+
+export const UserProfile = styled.div`
+position: absolute;
+width: 140.05px;
+height: 178px;
+left: 139px;
+top: 119px;
+
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25)
+color: white;
+`
+
+export const UserImg = styled.img`
+position: absolute;
+width: 111.11px;
+height: 111.14px;
+left: 19px;
+top: 4.52px;
+
+
+border-radius: 54px;
+`
+
+
+
+export const BtnEdit =  styled.button`
+
+
+box-sizing: border-box;
+
+position: absolute;
+width: 94.87px;
+height: 28.01px;
+left: 28px;
+top: 143px;
+
+background: #64C27B;
+border: 1px solid #000000;
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+border-radius: 40.6599px
+;`
+
+export const Texto = styled.div`
+font-family: 'Avenir Next';
+font-style: normal;
+font-weight: 700;
+font-size: 10.8426px;
+line-height: 15px;
+
+color: white`
