22a126ea3a56cb9eb0773eaf086e07ed2f108b77
tonymg1 <tonymartingaldeano@gmail.com>
2023-01-23 09:37:55 +0100

    wip

--- vite-project/src/router/Router.jsx
+++ vite-project/src/router/Router.jsx
@@ -1,6 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import {  Routes, Route, Navigate } from "react-router-dom";
-import { LoginPage, RegisterPage, AuthHomePage } from "../auth/pages/index.js";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { AuthHomePage } from "../auth/pages/index.js";
 import {
   ProfilePage,
   SearchPage,
@@ -8,64 +8,56 @@ import {
   HomePage,
   LikedPlayList,
 } from "../pages";
-import { useContext } from "react";
 import { EditProfilePage } from "../pages/EditProfilePage.jsx";
 import { AuthProvider } from "../auth/authContext/AuthProvider";
-import { AuthContext } from "../auth/authContext/AuthContext.jsx";
-
-import { EditProfile } from "../components";
-import { AuthRecoveryPage } from "../auth/pages/AuthRecoveryPage.jsx";
 import { Layout } from "../pages/Layout.jsx";
-import { PrivateRoutes } from "./PrivateRoutes.jsx";
-import RedirectPage from "../pages/RedirectPage.jsx";
+
 
 
 const Router = () => {
   const { isAuthenticated } = useAuth0();
   console.log("islogged?Router: ", isAuthenticated);
-  
- //const { authState } = useContext(AuthContext);
-  //const { isLogged } = authState;
+
 
   return (
     <>
-    
-        <AuthProvider>
-          <Routes>
-            <Route index path="/auth" element={isAuthenticated? <Navigate to= "/"/> : <AuthHomePage />} />
 
-            <Route element={<Layout />}>
-              <Route path="/" element={isAuthenticated ? <HomePage /> : <Navigate to= "/auth"/>} />
-              <Route
+      <AuthProvider>
+        <Routes>
+          <Route index path="/auth" element={isAuthenticated ? <Navigate to="/" /> : <AuthHomePage />} />
+
+          <Route element={<Layout />}>
+            <Route path="/" element={isAuthenticated ? <HomePage /> : <Navigate to="/auth" />} />
+            <Route
               path="/profile"
-                          element={ isAuthenticated ?  <ProfilePage />  : <Navigate to= "/auth"/>
-               
-                }
-              />
-              <Route
-                path="/editProfile"
-                element={
-                
-                    <EditProfilePage />
-           
-                }
-              />
-              {/* <Route path="/profile/:editId" element={<EditProfile />} /> */}
+              element={isAuthenticated ? <ProfilePage /> : <Navigate to="/auth" />
 
-              <Route
-                path="/search"
-                element={<SearchPage />  }
-                 
-                    
-             
-              
-              />
-              <Route path="/wish" element={<WishPage />} />
-              <Route path="/liked" element={<LikedPlayList />} />
-            </Route>
-          </Routes>
-        </AuthProvider>
-      
+              }
+            />
+            <Route
+              path="/editProfile"
+              element={
+
+                <EditProfilePage />
+
+              }
+            />
+          
+
+            <Route
+              path="/search"
+              element={<SearchPage />}
+
+
+
+
+            />
+            <Route path="/wish" element={<WishPage />} />
+            <Route path="/liked" element={<LikedPlayList />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
+
     </>
   );
 };
