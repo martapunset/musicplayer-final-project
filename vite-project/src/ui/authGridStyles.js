c8f8c6b6d75a40192687d46e0c36f0ce5b8a9afc
Elirv <elizabeth.ramos101@hotmail.com>
2023-01-25 12:46:24 +0000

    layout

--- vite-project/src/ui/authGridStyles.js
+++ vite-project/src/ui/authGridStyles.js
@@ -19,17 +19,14 @@ code {
     monospace;
 }
 
-/*Grid Area First Mobile*/
-/*min 640px*/
 .containerLogin {
   background-image: url(${homeBackground});
   background-repeat: no-repeat; 
   background-size: cover;
   grid-template-areas: 
-                    "header header header header"
-                    "header header header header"
-                    "main main main main"
-                    "footer footer footer footer";                 
+                    "9header header header"
+                    "main main main"
+                    "footer footer footer";                 
   width: 100%;
   height: 100vh;
 }`
